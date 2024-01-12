487062c3cc57fe056c0521d15fc8fd402f66e6dc
teguhfirmansyah <teguhfirmansyah080103@gmail.com>
2024-01-12 15:36:38 +0700

    soal14

---
+++ soal14.php
@@ -0,0 +1,14 @@
+<?php
+$color = "merah";
+switch ($color) {
+    case "merah";
+    echo "BENER";
+    break;
+    case "hijau";
+    echo "SALAH";
+    break;
+    default;
+    echo "Warna tidak terdefinisi";
+    break;
+}
+?>
